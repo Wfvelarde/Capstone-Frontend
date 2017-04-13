d5e25e98aa7382283301312d2117d434a58f1e84
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2017-04-13 09:52:48 -0600

    Styling changes

--- app/components/status-change-locked.js
+++ app/components/status-change-locked.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     ajax: Ember.inject.service(),
     click: function(){
+        alert("The lock has been closed!");
         return this.get('ajax').put('https://sleepy-brushlands-11143.herokuapp.com/lockstatuses/58e69449a1556d0011718c10', {
             data: {
                 status: 'closed'
